b1da2cb9161caf1caf94dd512e274994ddfacfa0
Richard <richardvclam@gmail.com>
2016-10-09 20:37:44 -0700

    Project 3: Program Done

--- src/project_three/Computer.java
+++ src/project_three/Computer.java
@@ -1,13 +1,17 @@
 package project_three;
 
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashMap;
 
-public class Computer {
+public class Computer implements Serializable {
 
 	int humanWins, cpuWins, ties;
 	HashMap<Pattern, Integer> patterns;
+	Pattern currentPattern;
 	
 	public Computer() {
+		currentPattern = new Pattern("");
 		patterns = new HashMap<Pattern, Integer>();
 		humanWins = 0;
 		cpuWins = 0;
@@ -16,7 +20,8 @@ public class Computer {
 	
 	public void play(int playerMove) {
 		String[] choices = {"Fire", "Water", "Grass"};
-		int cpuMove = makeMove();
+		storePattern(playerMove);
+		int cpuMove = makePrediction();
 		
 		System.out.println("You played " + choices[playerMove - 1] + ".");
 		System.out.println("The computer played " + choices[cpuMove - 1] + ".");
@@ -33,8 +38,58 @@ public class Computer {
 		}
 	}
 	
-	public int makeMove() {
+	public int makePrediction() {
+		String[] choices = {"Fire", "Water", "Grass"};
+		if (patterns.containsKey(currentPattern)) {
+			//int[] a = combinePattern(currentPattern).getCount();
+			int[] a = currentPattern.getCount();
+			int humanMove = -1;
+			
+			int largest = -1;
+			ArrayList<Integer> largestIndex = new ArrayList<Integer>();
+			
+			for (int i = 0; i < a.length; i++) {
+				if (a[i] > largest) {
+					largest = a[i];
+				}
+			}
+			
+			for (int i = 0; i < a.length; i++) {
+				if (a[i] == largest) {
+					largestIndex.add(i);
+				}
+			}
+			
+			if (largestIndex.size() == 1) {
+				humanMove = largestIndex.get(0);
+			} else if (largestIndex.size() > 1) {
+				humanMove = largestIndex.get((int) (Math.random() * 2));
+			}
+			
+			System.out.println("Computer predicted you'll play " + choices[humanMove]);
+			
+			switch (humanMove) {
+				case 0:
+					return 2;
+				case 1:
+					return 3;
+				case 2:
+					return 1;
+			}
+
+		}
 		return (int)(Math.random() * 3) + 1;
+	}
+	
+	public Pattern combinePattern(Pattern pattern) {
+		if (patterns.size() > 1) {
+			for (Pattern p : patterns.keySet()) {
+				if (!p.equals(pattern)) {
+					return new Pattern(pattern.getPattern() + p.getPattern());
+				}
+			}
+		}
+		return pattern;
 	}
 	
 	public void displayScores() {
@@ -42,6 +97,35 @@ public class Computer {
 		System.out.println("Human Wins: " + humanWins);
 		System.out.println("Computer Wins: " + cpuWins);
 		System.out.println("Ties: " + ties);
+		System.out.println("Computer win percentage: " + ((double)cpuWins/(humanWins+cpuWins)));
+	}
+	
+	public void storePattern(int response) {
+		int patternLength = 4;
+		String[] choices = {"F", "W", "G"};
+		
+		String pattern = currentPattern.getPattern();
+		
+		if (pattern.length() < patternLength) {
+			pattern += choices[response - 1];
+			currentPattern = new Pattern(pattern);
+		} else {
+			currentPattern = new Pattern(pattern.substring(1) + choices[response - 1]);
+		}
+		
+		if (pattern.length() == patternLength) {
+			if (patterns.containsKey(currentPattern)) {
+				patterns.put(currentPattern, patterns.get(currentPattern) + 1);
+			} else {
+				patterns.put(currentPattern, 1);
+			}
+		}
+	}
+	
+	public void resetScores() {
+		humanWins = 0;
+		cpuWins = 0;
+		ties = 0;
 	}
 	
 }
--- src/project_three/Pattern.java
+++ src/project_three/Pattern.java
@@ -1,8 +1,10 @@
 package project_three;
 
-public class Pattern {
+import java.io.Serializable;
 
-	String pattern;
+public class Pattern implements Serializable {
+
+	private String pattern;
 	
 	public Pattern(String pattern) {
 		this.pattern = pattern;
@@ -10,6 +12,38 @@ public class Pattern {
 	
 	public String getPattern() {
 		return pattern;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (o instanceof Pattern) {
+			Pattern p = (Pattern) o;
+			return p.getPattern().equals(pattern);
+		}
+		return false;
+	}
+	
+	@Override
+	public int hashCode() {
+		return pattern.hashCode();
+	}
+	
+	public int[] getCount() {
+		int f = 0, w = 0, g = 0;
+		
+		for (int i = 0; i < pattern.length(); i++) {
+			char c = pattern.charAt(i);
+			if (c == 'F') {
+				f++;
+			} else if (c == 'W') {
+				w++;
+			} else {
+				g++;
+			}
+		}
+		
+		int[] res = {f, w, g};
+		return res;
 	}
 
 }
